7b903b0b5de7ed78a45e10f207f498ed31e26e12
IamMattHenry <matthenry1005200@gmail.com>
2025-04-03 22:46:59 +0800

    t

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        { src: "public/main-resources/*.html", dest: "" }  // Copy all HTML to dist/
-      ]
-    }),
   ],
 })
