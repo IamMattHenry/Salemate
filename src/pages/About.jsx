c5bf8b63b26e5a4f283a04acc391755a41b1f916
rickygalanzajr13 <rickygalanzajr@gmail.com>
2025-04-06 23:58:59 +0800

    gyat

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,22 +25,22 @@ const About = () => {
       <div className="flex flex-col items-center">
         <HomeRoutes />
         <HomePanel> 
-        <div className="-mt-13  max-w-[1440px] mx-auto md:px-12 h-[1000px] bg-white border border-gray-100 rounded-3xl shadow-lg dark:bg-white-800 dark:border-white-700">
+        <div className="max-w-[1440px] mx-auto md:px-20 h-[1000px] bg-white border border-gray-100 rounded-3xl shadow-lg dark:bg-white-800 dark:border-white-700">
             <h2 className="text-900 text-5xl pt-15 dark:text-white font-bold font-league spartan text-center">Our Story</h2>
-            <p className="p-7 font-lato text-2xl md:text-3x1 text-center">At Salemate, we don’t just build sales tools we create  solutions that empower businesses to thrive. Born from the frustration of clunky, outdated systems. Salemate was designed to turn sales chaos into clarity, one smart feature at a time.</p>
+            <p className="p-7 font-lato text-2xl tracking-tight text-center">At Salemate, we don’t just build sales tools we create  solutions that empower businesses to thrive. Born from the frustration of clunky, outdated systems. Salemate was designed to turn sales chaos into clarity, one smart feature at a time.</p>
             <hr className="h-px my-7 w-200 mx-auto bg-gray-500 border-0 rounded-lg dark:bg-gray-400"></hr> 
 
-              <div className="flex mt-15">
-                <img className="h-100 w-[40rem] rounded-xl" src="\5.jpg" alt="team"></img>
+              <div className="flex mt-20">
+                  <h2 className="text-4xl font-bold font-league spartan text-center dark:text-white mt-10 ml-">How did Salemate come to be?</h2>
 
-                <div className="px-3 flex flex-col justify-center font-lato">
-                  <h2 className="text-4xl font-bold font-league spartan text-center dark:text-white -mt-3">The Journey to Simplicity</h2>
-                <p className=" text-2xl font-lato text-left dark:text-gray-300 p-7">It all started in our classrooms. Watching fellow entrepreneurship students struggle with clunky spreadsheets, lost sales data, and late-night formula errors, we asked: "Why does sales tracking have to be this hard?"
-                <br></br>
-                <br></br>
-                “After months of testing and feedback, we created Salemate – a simple way to replace spreadsheet headaches. No lost leads, no formula fails. Just more time to grow your business."</p>
-          </div>
-          </div>
+                <div className="px- flex flex-col tracking-tight font-lato">
+                  
+                  <p className=" text-2xl text-justify font-lato text-left dark:text-gray-300 ml-10 mr-15">It all started in our classrooms. Watching fellow entrepreneurship students struggle with clunky spreadsheets, lost sales data, and  late-night formula errors, we asked: "Why does sales tracking have to be this hard?"
+                  <br></br>
+                  <br></br>
+                  “After months of testing and feedback, we created Salemate – a simple way to replace spreadsheet headaches. No lost leads, no formula fails. Just more time to grow your business."</p>
+                </div>
+             </div>
           </div>
         </HomePanel>
       
