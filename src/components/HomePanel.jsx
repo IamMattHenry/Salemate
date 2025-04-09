32383f88a8f8f18b410183229aca70a32e97abc1
rickygalanzajr13 <rickygalanzajr@gmail.com>
2025-04-09 18:03:02 +0800

    terms-ni-reke

--- src/components/HomePanel.jsx
+++ src/components/HomePanel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const HomePanel = ({children}) => {
   return (
-    <div className="bg-whitesm/40 backdrop-invert-0 backdrop-opacity-10 block mx-auto rounded-3xl md:w-11/12 md:p-12 h-auto">
+    <div className="bg-whitesm/60 backdrop-invert-0 backdrop-opacity-10 block rounded-3xl w-[1300px] mx-auto md:px-12">
       {children}
     </div>
   )
