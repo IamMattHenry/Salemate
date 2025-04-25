9fd6adf344bad34ede2642c890cad9ea2ab03f90
IamMattHenry <matthenry1005200@gmail.com>
2025-04-25 17:09:26 +0800

    fix signin and signup design

--- src/pages/Home/SignIn.jsx
+++ src/pages/Home/SignIn.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { BsBoxArrowRight, BsAt, BsFillLockFill } from "react-icons/bs";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import firebaseApp from "../../firebaseConfig"; // Adjust the path to your Firebase config file
 
@@ -8,20 +10,16 @@ function SignIn() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
   const auth = getAuth(firebaseApp);
 
-  const togglePasswordVisibility = () => {
-    const passwordInput = document.getElementById("password");
-    const toggleIcon = document.getElementById("togglePassword");
-    if (passwordInput.type === "password") {
-      passwordInput.type = "text";
-      toggleIcon.innerHTML =
-        "<path d='M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0'/> <path d='M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8m8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7'/>";
-    } else {
-      passwordInput.type = "password";
-      toggleIcon.innerHTML =
-        "<path d='m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z'/><path d='M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z'/>";
+  const togglePasswordVisibility = (field) => {
+    if (field === "password") {
+      setPasswordVisible(!passwordVisible);
+    } else if (field === "confirmPassword") {
+      setConfirmPasswordVisible(!confirmPasswordVisible);
     }
   };
 
@@ -31,7 +29,11 @@ function SignIn() {
 
     try {
       // Sign in the user with email and password
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
       console.log("User signed in:", user);
@@ -51,67 +53,56 @@ function SignIn() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div className="form w-96 md:w-6/12 lg:w-4/12 h-auto block mx-auto my-12">
-          <div className="form-box bg-yellowb flex flex-col items-center font-lato h-full space-y-5 py-5 min-w-0 rounded-3xl shadow-2xl">
+        <div className="w-96 md:w-6/12 lg:w-4/12 h-auto block mx-auto my-12">
+          <div className="bg-yellowsm/20 flex flex-col items-center font-lato h-full space-y-5 py-5 rounded-3xl shadow-2xl">
             <div className="icon">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                fill="currentColor"
-                className="bi bi-box-arrow-in-right"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-                />
-              </svg>
+              <BsBoxArrowRight size={32} />
             </div>
             <div className="signin text-center">
               <h3 className="text-3xl text-black mb-2">Sign In</h3>
-              <p className="text-sm text-black">
+              <p className="text-[1rem] text-black">
                 Make sure to create your account first before signing in
               </p>
             </div>
             <div className="signin-inp space-y-3">
-              <div className="relative w-full max-w-xs mx-auto">
+              <div className="relative w-full mx-auto">
+                <BsAt
+                  className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+                  size={16}
+                />
                 <input
                   type="email"
                   placeholder="Email"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                  className="pl-4 pr-4 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500 bg-white"
+                  className="pl-10 pr-4 py-2 w-full rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-500 bg-white"
                   required
                 />
               </div>
-              <div className="relative w-full max-w-xs mx-auto">
+              <div className="relative w-full mx-auto">
+                <BsFillLockFill
+                  className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+                  size={16}
+                />
                 <input
                   id="password"
-                  type="password"
+                  type={confirmPasswordVisible ? "text" : "password"}
                   placeholder="Password"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
-                  className="pl-4 pr-10 py-2 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500 bg-white"
+                  className="pl-10 pr-10 py-2 w-84 rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-500 bg-white"
                   required
                 />
-                <svg
-                  id="togglePassword"
+                <div
                   className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                  onClick={togglePasswordVisibility}
+                  onClick={() => togglePasswordVisibility("confirmPassword")}
                 >
-                  <path d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7 7 0 0 0 2.79-.588M5.21 3.088A7 7 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474z" />
-                  <path d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12z" />
-                </svg>
+                  {confirmPasswordVisible ? (
+                    <FaEyeSlash size={20} />
+                  ) : (
+                    <FaEye size={20} />
+                  )}
+                </div>
               </div>
               {error && <p className="text-red-500 text-sm">{error}</p>}
               <div className="w-full text-right">
@@ -121,17 +112,17 @@ function SignIn() {
               </div>
               <div className="text-center">
                 <button
-                  className="font-lato font-bold mt-4 bg-black py-3 px-5 rounded-lg text-whitesm cursor-pointer"
+                  className="font-lato font-bold mt-4 bg-black py-3 px-5 rounded-2xl text-whitesm cursor-pointer"
                   type="submit"
                 >
                   Sign In
                 </button>
               </div>
               <div className="w-full text-center">
-                <p className="text-black text-sm font-lato">
+                <p className="text-black text-[.8rem] font-latrue">
                   Don't have an account yet?{" "}
                   <a
-                    className="text-sm font-lato underline cursor-pointer"
+                    className="text-[.8rem] font-latrue underline cursor-pointer"
                     href="/signup"
                     target="_self"
                   >
--- src/pages/Home/SignUp.jsx
+++ src/pages/Home/SignUp.jsx
@@ -1,7 +1,12 @@
 // src/components/SignUp.jsx
 import React, { useState } from "react";
 import { FaEye, FaEyeSlash, FaUserAlt } from "react-icons/fa";
-import { BsBoxArrowRight, BsAt, BsFillLockFill, BsPeopleFill } from "react-icons/bs";
+import {
+  BsBoxArrowRight,
+  BsAt,
+  BsFillLockFill,
+  BsPeopleFill,
+} from "react-icons/bs";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 import firebaseApp from "../../firebaseConfig"; // Adjust the path to your Firebase config file
@@ -32,14 +37,19 @@ function SignUp() {
   const validateFields = () => {
     const nameRegex = /^[a-zA-Z\s]+$/; // Allow letters and spaces
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Valid email format
-    const passwordRegex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/; // At least 6 characters, 1 numeric, 1 special character
+    const passwordRegex =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/; // At least 6 characters, 1 numeric, 1 special character
 
     if (!firstName || !nameRegex.test(firstName)) {
-      setError("First Name is required and must not contain special characters or numbers.");
+      setError(
+        "First Name is required and must not contain special characters or numbers."
+      );
       return false;
     }
     if (!lastName || !nameRegex.test(lastName)) {
-      setError("Last Name is required and must not contain special characters or numbers.");
+      setError(
+        "Last Name is required and must not contain special characters or numbers."
+      );
       return false;
     }
     if (!email || !emailRegex.test(email)) {
@@ -51,7 +61,9 @@ function SignUp() {
       return false;
     }
     if (!password || !passwordRegex.test(password)) {
-      setError("Password must be at least 6 characters long and include a number and a special character.");
+      setError(
+        "Password must be at least 6 characters long and include a number and a special character."
+      );
       return false;
     }
     if (password !== confirmPassword) {
@@ -72,7 +84,11 @@ function SignUp() {
 
     try {
       // Create user with email and password
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
 
       // Save additional user data to Firestore
@@ -87,7 +103,9 @@ function SignUp() {
       console.log("User registered:", user);
     } catch (err) {
       if (err.code === "auth/email-already-in-use") {
-        setError("This email is already registered. Please use a different email.");
+        setError(
+          "This email is already registered. Please use a different email."
+        );
       } else {
         setError(err.message);
       }
@@ -157,9 +175,13 @@ function SignUp() {
                 <select
                   value={department}
                   onChange={(e) => setDepartment(e.target.value)}
-                  className="pl-10 pr-4 py-2 w-full rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-500 bg-white"
+                  className={`py-3 pl-[2.2rem] border-r-8 border-transparent outline outline-transparent w-full rounded-2xl focus:outline-none focus:ring-2 focus:ring-yellow-500 bg-white ${
+                    department === '' ? 'text-gray-500' : 'text-black'
+                  }`}
                 >
-                  <option value="">Select Department</option>
+                  <option value="" disabled>
+                    Select Department
+                  </option>
                   <option value="Financial">Financial</option>
                   <option value="Marketing">Marketing</option>
                   <option value="Production">Production</option>
@@ -182,7 +204,11 @@ function SignUp() {
                   className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
                   onClick={() => togglePasswordVisibility("password")}
                 >
-                  {passwordVisible ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+                  {passwordVisible ? (
+                    <FaEyeSlash size={20} />
+                  ) : (
+                    <FaEye size={20} />
+                  )}
                 </div>
               </div>
               <div className="relative w-full max-w-xs mx-auto">
@@ -202,14 +228,18 @@ function SignUp() {
                   className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
                   onClick={() => togglePasswordVisibility("confirmPassword")}
                 >
-                  {confirmPasswordVisible ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
+                  {confirmPasswordVisible ? (
+                    <FaEyeSlash size={20} />
+                  ) : (
+                    <FaEye size={20} />
+                  )}
                 </div>
               </div>
               {error && <p className="text-red-500 text-sm">{error}</p>}
               {success && <p className="text-green-500 text-sm">{success}</p>}
               <div className="text-center">
                 <button
-                  className="font-lato font-bold mt-4 bg-black py-3 px-5 rounded-lg text-whitesm"
+                  className="font-lato font-bold mt-4 bg-black py-3 px-5 rounded-2xl text-whitesm"
                   type="submit"
                 >
                   Continue
